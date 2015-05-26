a5a1f5f2a9b3768b18610e3ccea1f00588d315c8
thierrygosselin <thierrygosselin@me.com>
2015-05-26 18:45:17 -0400

    edits

--- R/write_genepop.R
+++ R/write_genepop.R
@@ -1,5 +1,5 @@
 # Write a genepop file from STACKS haplotype file
-if(getRversion() >= "2.15.1")  utils::globalVariables(c(`Catalog ID`,"Cnt", "HAPLOTYPES", "SAMPLES", "ALLELE_1", "ALLELE_2", "GENOTYPE"))
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("Catalog ID","Cnt", "HAPLOTYPES", "SAMPLES", "ALLELE_1", "ALLELE_2", "GENOTYPE"))
 
 
 
