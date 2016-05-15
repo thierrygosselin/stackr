bb49ae0dee28c5c8ed81950c6fd72e0901fdfd7f
Thierry Gosselin <thierrygosselin@icloud.com>
2016-05-15 11:41:30 +1000

    extracted the function to write genepop, now independent function

---
+++ R/write_genepop.R
@@ -0,0 +1,122 @@
+# write a genepop file from a tidy data frame
+
+#' @name write_genepop
+#' @title Used internally in stackr to write a genepop file from a tidy data frame
+#' @description Write a genepop file from a tidy data frame
+#' @param data A file or object in the global environment containing at least 
+#' these 4 columns: 
+#' \code{INDIVIDUALS}, \code{POP_ID} (that refers to any grouping of individuals.), 
+#' \code{MARKERS} and \code{GENOTYPES} in a tidy format. During import, 
+#' only those columns names will be kept. 
+#' @param sep (optional) A character string separating alleles. 
+#' Default: \code{sep = NULL}.
+#' @param pop.levels (optional, string) A character string with your populations ordered.
+#' Default: \code{pop.levels = NULL}.
+#' @param genepop.header The first line of the Genepop file.
+#' Default: \code{genepop.header = "my firt genepop"}.
+#' @param markers.line.format (optional, character) You can write the markers 
+#' on a single line separated by commas \code{markers.line.format = "line"}, 
+#' or have markers on a separate line, i.e. in one column, 
+#' \code{markers.line.format = "column"} 
+#' (not very useful with thousands of markers). 
+#' Default: \code{markers.line.format = "line"}.
+#' @param filename The name of the file written to the working directory.
+#' Use the extension ".gen" at the end. 
+#' Default: \code{filename = "genepop.gen"}.
+#' @param ... other parameters passed to the function.
+#' @return A genepop file is saved to the working directory. 
+#' @details Details for the sep argument
+#' This character is directly used in regular expressions using strigi. 
+#' Some characters need to be preceeded by double backslashes \code{\\}. 
+#' For instance, "/" works but "|" must be coded as "\\|".
+#' @export
+#' @rdname write_genepop
+#' @import reshape2
+#' @import dplyr
+#' @import stringi
+#' @importFrom data.table fread
+
+#' @author Thierry Gosselin \email{thierrygosselin@@icloud.com}
+
+# to get rid of notes in build check
+# if(getRversion() >= "2.15.1") {
+#   utils::globalVariables(c("Catalog ID", "Catalog.ID", "Catalog.ID = LOCUS", 
+#                            "Catalog.ID = `Catalog ID`", "Cnt", "HAPLOTYPES", 
+#                            "SAMPLES", "ALLELES", 'A1', 'A2', 'COUNT', 
+#                            "GENOTYPE", "NUCLEOTIDES", "INDIVIDUALS", "POP_ID", 
+#                            "POLYMORPHISM", "POLYMORPHISM_MAX", "other", 
+#                            "strata", "hierarchy", "GROUP", ".", 'MARKERS', 
+#                            'MARKERS_ALLELES', 'STRATA'
+#   )
+#   )
+# }
+
+
+write_genepop <- function(data, sep, pop.levels, genepop.header, markers.line.format, filename, ...) {
+  
+  # Checking for missing and/or default arguments ******************************
+  if (missing(data)) stop("Input file necessary to write the genepop file is missing")
+  if (missing(sep)) sep <- NULL
+  if (missing(pop.levels)) pop.levels <- NULL
+  if (missing(genepop.header)) genepop.header <- "my firt genepop"
+  if (missing(markers.line.format)) markers.line.format <- "line"
+  if (missing(filename)) filename <- "genepop.gen"
+  
+  # Import data ---------------------------------------------------------------
+  if (is.vector(data) == TRUE) {
+    input <- data.table::fread(
+      input = data,
+      sep = "\t",
+      stringsAsFactors = FALSE, 
+      header = TRUE,
+      select = c("POP_ID", "INDIVIDUALS", "MARKERS", "GENOTYPE"),
+      showProgress = TRUE,
+      verbose = FALSE
+    ) %>% 
+      as_data_frame() 
+  } else {
+    input <- data %>% 
+      select(POP_ID, INDIVIDUALS, MARKERS, GENOTYPE)
+  }
+  
+  if (!is.null(sep)) {
+    input <- input %>% 
+      mutate(GENOTYPE = stri_replace_all_fixed(str = GENOTYPE, pattern = sep, replacement = "", vectorize_all = FALSE))
+  }
+  
+  # pop.levels -----------------------------------------------------------------
+  if (!is.null(pop.levels)) {
+    input <- input %>%
+      mutate(POP_ID = factor(POP_ID, levels = pop.levels, ordered =TRUE)) %>% 
+      arrange(POP_ID, INDIVIDUALS, MARKERS)
+  }
+  
+  # Create a marker vector  ------------------------------------------------
+  markers <- input %>% select(MARKERS) %>% distinct(MARKERS) %>% arrange(MARKERS)
+  markers <- markers$MARKERS
+  
+  # Wide format ----------------------------------------------------------------
+  input <- input %>%
+    arrange(MARKERS) %>% 
+    group_by(POP_ID, INDIVIDUALS) %>%
+    tidyr::spread(data = ., key = MARKERS, value = GENOTYPE) %>% 
+    arrange(POP_ID, INDIVIDUALS) %>%
+    ungroup() %>% 
+    mutate(INDIVIDUALS = paste(INDIVIDUALS, ",", sep = ""))
+  
+  # Write the file in genepop format -------------------------------------------
+  pop <- input$POP_ID # Create a population vector
+  input <- split(select(.data = input, -POP_ID), pop) # split genepop by populations
+  filename.connection <- file(filename, "w") # open the connection to the file
+  writeLines(text = genepop.header, con = filename.connection, sep = "\n") # write the genepop header
+  if (markers.line.format == "line") { # write the markers on a single line
+    writeLines(text = stri_paste(markers, sep = ",", collapse = ", "), con = filename.connection, sep = "\n") 
+  } else {# write the markers on a single column (separate lines)
+    writeLines(text = stri_paste(markers, sep = "\n"), con = filename.connection, sep = "\n")
+  }
+  close(filename.connection) # close the connection
+  for (i in 1:length(input)) {
+    write_delim(x = as.data.frame("pop"), path = filename, delim = "\n", append = TRUE, col_names = FALSE)
+    write_delim(x = input[[i]], path = filename, delim = " ", append = TRUE, col_names = FALSE)
+  }
+}
