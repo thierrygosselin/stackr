78173319ccb7c328969cfc54e1c9d15d69464062
Thierry Gosselin <thierrygosselin@icloud.com>
2016-06-06 15:30:57 -0400

    updated vignettes

--- vignettes/vignette_imputations_parallel.R
+++
@@ -1,4 +0,0 @@
-## ---- echo = FALSE, message = FALSE--------------------------------------
-  knitr::opts_chunk$set(collapse = T, comment = "#>")
-  options(tibble.print_min = 4L, tibble.print_max = 4L)
-
