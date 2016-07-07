0042d165179e962255a529566b08390c5ca9b2df
Anne-Laure Ferchaud <annelaureferchaud@gmail.com>
2016-07-07 16:46:33 -0400

    fix typo

--- R/missing_visualization.R
+++ R/missing_visualization.R
@@ -2,8 +2,8 @@
 #' @title Visualize missing genotypes in genomic data set.
 #' @description Use this function to visualize pattern of missing data.
 #'  \itemize{
-#'    \item \strong{Imput file:} various file format are supported 
-#'    (see \code{data} argument below)
+#'    \item \strong{Input file:} various file formats are supported 
+#'    (see \code{data} arguments below)
 #'    \item \strong{Filters:} genotypes, markers, individuals and populations can be 
 #'   filtered and/or selected in several ways using blacklist,
 #'   whitelist and other arguments
