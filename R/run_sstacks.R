40075e62ea7bbf777e789303af6f50117aecf544
Thierry Gosselin <thierrygosselin@icloud.com>
2016-12-06 18:11:43 -0500

    edit

--- R/run_sstacks.R
+++ R/run_sstacks.R
@@ -66,9 +66,10 @@
 
 #' @rdname run_sstacks
 #' @export
-#' @import stringi
-#' @import dplyr
-#' @import readr
+#' @importFrom stringi stri_join stri_replace_all_fixed stri_sub
+#' @importFrom dplyr mutate filter distinct 
+#' @importFrom purrr keep
+#' @importFrom tibble data_frame
 
 #' @return \href{http://catchenlab.life.illinois.edu/stacks/comp/sstacks.php}{sstacks}
 #' returns a \code{.matches.tsv.gz file for each sample}
@@ -87,12 +88,6 @@
 #' x = FALSE, gapped = FALSE)
 #' }
 
-# required to pass the R CMD check and have 'no visible binding for global variable'
-if (getRversion() >= "2.15.1") {
-  utils::globalVariables(
-    c("INDIVIDUALS_REP")
-  )
-}
 
 #' @seealso 
 #' \href{http://catchenlab.life.illinois.edu/stacks/comp/sstacks.php}{sstacks} 
@@ -124,16 +119,16 @@ run_sstacks <- function(
   cat("#######################################################################\n")
   
   # Check directory ------------------------------------------------------------
-  if(!dir.exists(input.path)) dir.create(input.path)
-  if(!dir.exists("09_log_files")) dir.create("09_log_files")
+  if (!dir.exists(input.path)) dir.create(input.path)
+  if (!dir.exists("09_log_files")) dir.create("09_log_files")
   
   # sstacks options ------------------------------------------------------------
   
   # p: enable parallel execution with num_threads threads.
-  p <- stri_paste("-p ", p)
+  p <- stringi::stri_join("-p ", p)
   
   # b: MySQL ID of this batch.
-  b <- stri_paste("-b ", b)
+  b <- stringi::stri_join("-b ", b)
   
   # c: TSV file from which to load the catalog loci.
   # setwd("/Volumes/Drobo/esturgeon_drobo/sturgeon_manitoba/07-stacks_rx")
@@ -148,43 +143,43 @@ run_sstacks <- function(
         arguments, usually it starts with batch_1")
   }
   
-  c <- stri_paste("-c ", input.path, "/", catalog.prefix)
+  c <- stringi::stri_join("-c ", input.path, "/", catalog.prefix)
   
   # o: output path to write results.
-  o <- stri_paste("-o ", shQuote(o))
+  o <- stringi::stri_join("-o ", shQuote(o))
   
   # g: base matching on genomic location, not sequence identity.
-  if(g) {
-    g <- stri_paste("-g ")
+  if (g) {
+    g <- stringi::stri_join("-g ")
   } else {
     g <- ""
   }
   
   # x: don't verify haplotype of matching locus.
-  if(x) {
-    x <- stri_paste("-x ")
+  if (x) {
+    x <- stringi::stri_join("-x ")
   } else {
     x <- ""
   }  
   
   # v: print program version.
-  if(v) {
-    v <- stri_paste("-v ")
+  if (v) {
+    v <- stringi::stri_join("-v ")
   } else {
     v <- ""
   }
   
   # h: display this help messsage.
   if (h) {
-    h <- stri_paste("-h ")
+    h <- stringi::stri_join("-h ")
   } else {
     h <- ""
   }
   
   # Gapped assembly options ---------------------------------------------------
   # --gapped: preform gapped alignments between stacks.
-  if(gapped) {
-    gapped <- stri_paste("--gapped ")
+  if (gapped) {
+    gapped <- stringi::stri_join("--gapped ")
   } else {
     gapped <- ""
   } 
@@ -192,75 +187,71 @@ run_sstacks <- function(
   # s: filename prefix from which to load sample loci---------------------------
   
   # Get the samples in the folder
-  samples.in.folder <- data_frame(INDIVIDUALS_REP = list.files(input.path)) %>% 
-    filter(!grepl("batch", INDIVIDUALS_REP))
+  samples.in.folder <- tibble::data_frame(INDIVIDUALS_REP = list.files(input.path)) %>% 
+    dplyr::filter(!grepl("batch", INDIVIDUALS_REP))
   
   # Search for those already matched
   samples.matched <- samples.in.folder %>% 
-    filter(grepl("matches", INDIVIDUALS_REP)) %>% 
-    mutate(INDIVIDUALS_REP = stri_replace_all_fixed(
+    dplyr::filter(grepl("matches", INDIVIDUALS_REP)) %>% 
+    dplyr::mutate(INDIVIDUALS_REP = stringi::stri_replace_all_fixed(
       str = INDIVIDUALS_REP, 
       pattern = ".matches.tsv.gz", replacement = "", 
       vectorized_all = FALSE)
     ) %>% 
-    distinct(INDIVIDUALS_REP)
+    dplyr::distinct(INDIVIDUALS_REP)
   
-  message(stri_paste("Samples in folder already matched to the catalog: ", 
+  message(stringi::stri_join("Samples in folder already matched to the catalog: ", 
                      length(samples.matched$INDIVIDUALS_REP)))
   
   # Get the name of samples that need to be match to the catalog
   samples.to.match <- samples.in.folder %>% 
-    filter(grepl("alleles", INDIVIDUALS_REP)) %>% 
-    mutate(INDIVIDUALS_REP = stri_replace_all_fixed(
+    dplyr::filter(grepl("alleles", INDIVIDUALS_REP)) %>% 
+    dplyr::mutate(INDIVIDUALS_REP = stringi::stri_replace_all_fixed(
       str = INDIVIDUALS_REP, 
       pattern = ".alleles.tsv.gz", replacement = "", 
       vectorized_all = FALSE)
     ) %>% 
-    distinct(INDIVIDUALS_REP) %>% 
-    filter(!INDIVIDUALS_REP %in% samples.matched$INDIVIDUALS_REP)
+    dplyr::distinct(INDIVIDUALS_REP) %>% 
+    dplyr::filter(!INDIVIDUALS_REP %in% samples.matched$INDIVIDUALS_REP)
   
-  message(stri_paste("Samples in folder not matched to the catalog: ", length(samples.to.match$INDIVIDUALS_REP)))
+  message(stringi::stri_join("Samples in folder not matched to the catalog: ", length(samples.to.match$INDIVIDUALS_REP)))
   
   
   if (is.null(sample.list)) {
-    s <- stri_paste("-s ", shQuote(stri_paste(input.path, "/", samples.to.match$INDIVIDUALS_REP)))
-    message(stri_paste("Matching ", length(samples.to.match$INDIVIDUALS_REP), " sample(s) to the catalog..."))
+    s <- stringi::stri_join("-s ", shQuote(stringi::stri_join(input.path, "/", samples.to.match$INDIVIDUALS_REP)))
+    message(stringi::stri_join("Matching ", length(samples.to.match$INDIVIDUALS_REP), " sample(s) to the catalog..."))
   } else {
     sample.list.before <- sample.list
     sample.list <- purrr::keep(.x = sample.list, .p = sample.list %in% samples.to.match$INDIVIDUALS_REP)
-    sample.list <- stri_paste(input.path, "/", sample.list)
-    s <- stri_paste("-s ", shQuote(sample.list))
+    sample.list <- stringi::stri_join(input.path, "/", sample.list)
+    s <- stringi::stri_join("-s ", shQuote(sample.list))
     
-    samples.to.remove <- length(sample.list) -length(sample.list.before)
+    samples.to.remove <- length(sample.list) - length(sample.list.before)
     if (samples.to.remove != 0) {
-      message(stri_paste("Removed ", samples.to.remove, " problematic sample(s) from the samples list to match"))
+      message(stringi::stri_join("Removed ", samples.to.remove, " problematic sample(s) from the samples list to match"))
     }
-    message(stri_paste("Matching ", length(sample.list), " sample(s) to the catalog..."))
+    message(stringi::stri_join("Matching ", length(sample.list), " sample(s) to the catalog..."))
   }
   
   # logs files -----------------------------------------------------------------
-  file.date.time <- stri_replace_all_fixed(Sys.time(), pattern = " EDT", replacement = "")
-  file.date.time <- stri_replace_all_fixed(file.date.time, pattern = c("-", " ", ":"), replacement = c("", "@", ""), vectorize_all = FALSE)
-  file.date.time <- stri_sub(file.date.time, from = 1, to = 13)
+  file.date.time <- stringi::stri_replace_all_fixed(Sys.time(), pattern = " EDT", replacement = "")
+  file.date.time <- stringi::stri_replace_all_fixed(file.date.time, pattern = c("-", " ", ":"), replacement = c("", "@", ""), vectorize_all = FALSE)
+  file.date.time <- stringi::stri_sub(file.date.time, from = 1, to = 13)
   
-  log.file <- stri_paste("09_log_files/sstacks_", file.date.time,".log")
-  message(stri_paste("For progress, look in the log file: ", log.file))
+  log.file <- stringi::stri_join("09_log_files/sstacks_", file.date.time,".log")
+  message(stringi::stri_join("For progress, look in the log file: ", log.file))
   
   # command args ---------------------------------------------------------------
-  command.arguments <- c(
-    p, b, c, s, o, g, x, v, h, gapped
-  )
+  command.arguments <- c(p, b, c, s, o, g, x, v, h, gapped)
   
   # command
-  system.time(
     system2(
       command = "sstacks", 
       args = command.arguments, 
-      stdout = log.file, 
+      # stdout = log.file, 
       stderr = log.file
     )
-  )
-  
+
   # # transfer back to s3
   # if (transfer.s3) {
   #   cstacks.files.to.s3 <- list.files(path = sample.list.path, pattern = individual, full.names = FALSE)
