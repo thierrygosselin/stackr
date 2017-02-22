8a61d3740fde838ca0d1c23569e4a54d8c7e2188
Thierry Gosselin <thierrygosselin@icloud.com>
2017-02-22 15:07:32 -0500

    * Better code for the progress bar (Linux and Mac only) that now shows an ETA along the progress.

--- R/stackr_parallel.R
+++ R/stackr_parallel.R
@@ -89,93 +89,6 @@ mclapply_win <- function(
 }#End mclapply_win
 
 
-# mclapply with progress bar ---------------------------------------------------
-#' @title mclapply_progress_bar
-#' @description progress bar
-#' @param X see \pkg{parallel} \code{\link{mclapply}}
-#' @param FUN see \pkg{parallel} \code{\link{mclapply}}
-#' @param ... see \pkg{parallel} \code{\link{mclapply}}
-# @param mc.preschedule see \pkg{parallel} \code{\link{mclapply}}
-# @param mc.set.seed see \pkg{parallel} \code{\link{mclapply}}
-# @param mc.silent see \pkg{parallel} \code{\link{mclapply}}
-#' @param mc.cores see \pkg{parallel} \code{\link{mclapply}}
-# @param mc.cleanup see \pkg{parallel} \code{\link{mclapply}}
-# @param mc.allow.recursive see \pkg{parallel} \code{\link{mclapply}}
-# @param style see \pkg{utils} \code{\link{txtProgressBar}}
-#' @rdname mclapply_progress_bar
-#' @export
-#' @keywords internal
-#' @importFrom future plan futureCall value multiprocess
-#' @importFrom parallel mclapply
-#' @importFrom utils txtProgressBar setTxtProgressBar
-#' @importFrom pryr named_dots dots
-PORT = 6011
-mclapply_progress_bar <- function(
-  X, FUN, ..., mc.cores = parallel::detectCores() - 1, ignore.interactive = FALSE
-) {
-  # mc.preschedule = TRUE, mc.set.seed = TRUE,
-  # mc.silent = FALSE, mc.cores = getOption("mc.cores", 2L), mc.cleanup = TRUE,
-  # mc.allow.recursive = TRUE  
-  
-  # new plan
-  first.plan <- future::plan(strategy = "list")
-  on.exit(future::plan(strategy = first.plan))
-  future::plan(future::multiprocess)
-  
-  if (!is.vector(X) || is.object(X)) {
-    X <- as.list(X)
-  }
-  
-  # Account for interactive mode
-  if (!interactive() & !ignore.interactive) {
-    parallel::mclapply(X, FUN, ..., mc.cores = mc.cores)
-  }
-  
-  monitoring <- future::futureCall(function(X, FUN, ..., mc.cores) {
-    socket.server <- socketConnection(
-      open = "wb", port = PORT, blocking = TRUE, server = TRUE)
-    tryCatch(
-      result <- parallel::mclapply(X, function(...) {
-        res <- FUN(...)
-        writeBin(1, socket.server)
-        return(res)
-      }, ..., mc.cores = mc.cores),
-      finally = {close(socket.server)}
-    )
-    return(result)
-  },
-  args = list(X, FUN, ..., mc.cores = mc.cores),
-  envir = environment(),
-  # globals = TRUE
-  globals = list(PORT = PORT)
-  )
-  
-  length <- length(X)
-  pb <- utils::txtProgressBar(min = 0, max = length, style = 3)
-  utils::setTxtProgressBar(pb, 0)
-  progress <- 0
-  
-  # Creating socket client and updating pb
-  socket.active <- FALSE
-  while (!socket.active) {
-    Sys.sleep(0.5)
-    try(socket.connection <- socketConnection(
-      open = "rb", port = PORT, blocking = TRUE, server = FALSE)
-      , silent = TRUE)
-    if (exists("socket.connection")) {
-      socket.active <- TRUE
-      while (progress < length) {
-        readBin(socket.connection, "double")
-        progress <- progress + 1
-        utils::setTxtProgressBar(pb, progress)
-      }
-      close(socket.connection)
-    }
-  }
-  
-  return(future::value(monitoring))
-}
-
 # .stackr_parallel--------------------------------------------------------------
 # Overwrite the serial version of mclapply on Windows only
 # @name .stackr_parallel
@@ -185,18 +98,18 @@ mclapply_progress_bar <- function(
 # @inheritParams parallel::mclapply
 #' @return For mclapply, a list of the same length as X and named by X.
 # @importFrom parallel detectCores makeCluster clusterExport mclapply parLapply stopCluster
-# @importFrom pbmcapply pbmclapply
+#' @importFrom pbmcapply pbmclapply
 #' @rdname stackr_parallel
 #' @keywords internal
 #' @export
 .stackr_parallel <- switch(
   Sys.info()[['sysname']],
   Windows = {mclapply_win},
-  Linux   = {mclapply_progress_bar},
+  # Linux   = {mclapply_progress_bar},
   # Linux   = {parallel::mclapply},
-  # Linux   = {pbmcapply::pbmclapply},
-  Darwin  = {mclapply_progress_bar}
+  Linux   = {pbmcapply::pbmclapply},
+  # Darwin  = {mclapply_progress_bar}
   # Darwin  = {parallel::mclapply}
-  # Darwin  = {pbmcapply::pbmclapply}
+  Darwin  = {pbmcapply::pbmclapply}
 )
 
